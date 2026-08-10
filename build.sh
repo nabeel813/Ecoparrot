85a9fcd46c7f57019b0b2608c1642fbde7a8b7d6
Nabeel Rahman <nblnbl224@gmail.com>
2026-08-10 11:44:42 +0530

    Reset admin password

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
+python manage.py shell -c "from django.contrib.auth.models import User; u,created = User.objects.get_or_create(username='admin', defaults={'email':'nblnbl224@gmail.com'}); u.set_password('nabeel123'); u.is_superuser=True; u.is_staff=True; u.save()"
